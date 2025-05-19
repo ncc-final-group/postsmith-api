08b350de2357b719f8f3fb0312f594da9f97ce07
ChoiWonOong <dnd5301@gmail.com>
2025-05-19 14:42:58 +0900

    first commit

---
+++ src/main/java/com/example/postsmist_api/PostsmistApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.postsmist_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostsmistApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostsmistApiApplication.class, args);
+	}
+
+}
