6a0834584f5460827cdcb8f84f45298071a0343b
chauid <dlfdl28@naver.com>
2025-06-20 20:28:59 +0900

    [0.0.3] 배포

--- src/main/java/com/postsmith/api/domain/login/config/OAuthSuccessHandler.java
+++ src/main/java/com/postsmith/api/domain/login/config/OAuthSuccessHandler.java
@@ -22,7 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 public class OAuthSuccessHandler implements AuthenticationSuccessHandler {
 	@Value("${url.base}")
 	private String baseUrl;
-
+	
 	@Value("${url.domain}")
 	private String domain;
 
